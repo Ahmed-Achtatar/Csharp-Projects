08cda4ee9e4e9150e943e3023061e63787ec60c8
Ahmed Achtatar <ahmed.achtatar@usmba.ac.ma>
2021-10-25 17:10:49 +0100

    edit test

---
+++ project1/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Character
+    {
+        public int Health { get; set; }
+
+        public Character(int a)
+        {
+            Health = a;
+        }
+    }
+}
---
+++ project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character S = new(15);
+            Console.WriteLine(S.Health);
+        }
+    }
+}
