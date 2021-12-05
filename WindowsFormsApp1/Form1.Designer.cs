15ddd25ec55b80d46e86118bb90c43bb398b7bfa
Ahmed Achtatar <ahmed.achtatar@usmba.ac.ma>
2021-12-05 19:00:24 +0100

    everything is edited

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listEtudiant = new System.Windows.Forms.ComboBox();
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.Modifier = new System.Windows.Forms.Button();
+            this.Supprimer = new System.Windows.Forms.Button();
+            this.Annuler = new System.Windows.Forms.Button();
+            this.Valider = new System.Windows.Forms.Button();
+            this.nomTxtBox = new System.Windows.Forms.TextBox();
+            this.prenomTextBox = new System.Windows.Forms.TextBox();
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.villeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listEtudiant
+            // 
+            this.listEtudiant.FormattingEnabled = true;
+            this.listEtudiant.Location = new System.Drawing.Point(85, 69);
+            this.listEtudiant.Name = "listEtudiant";
+            this.listEtudiant.Size = new System.Drawing.Size(243, 21);
+            this.listEtudiant.TabIndex = 0;
+            this.listEtudiant.SelectedIndexChanged += new System.EventHandler(this.listEtudiant_SelectedIndexChanged);
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.Location = new System.Drawing.Point(12, 275);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.Ajouter.TabIndex = 1;
+            this.Ajouter.Text = "Ajouter";
+            this.Ajouter.UseVisualStyleBackColor = true;
+            this.Ajouter.Click += new System.EventHandler(this.Ajouter_Click);
+            // 
+            // Modifier
+            // 
+            this.Modifier.Location = new System.Drawing.Point(108, 275);
+            this.Modifier.Name = "Modifier";
+            this.Modifier.Size = new System.Drawing.Size(75, 23);
+            this.Modifier.TabIndex = 2;
+            this.Modifier.Text = "Modifier";
+            this.Modifier.UseVisualStyleBackColor = true;
+            this.Modifier.Click += new System.EventHandler(this.Modifier_Click);
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.Location = new System.Drawing.Point(207, 275);
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Size = new System.Drawing.Size(75, 23);
+            this.Supprimer.TabIndex = 3;
+            this.Supprimer.Text = "Supprimer";
+            this.Supprimer.UseVisualStyleBackColor = true;
+            this.Supprimer.Click += new System.EventHandler(this.Supprimer_Click);
+            // 
+            // Annuler
+            // 
+            this.Annuler.Location = new System.Drawing.Point(442, 275);
+            this.Annuler.Name = "Annuler";
+            this.Annuler.Size = new System.Drawing.Size(75, 23);
+            this.Annuler.TabIndex = 4;
+            this.Annuler.Text = "Annuler";
+            this.Annuler.UseVisualStyleBackColor = true;
+            this.Annuler.Click += new System.EventHandler(this.Annuler_Click);
+            // 
+            // Valider
+            // 
+            this.Valider.Location = new System.Drawing.Point(523, 275);
+            this.Valider.Name = "Valider";
+            this.Valider.Size = new System.Drawing.Size(75, 23);
+            this.Valider.TabIndex = 5;
+            this.Valider.Text = "Valider";
+            this.Valider.UseVisualStyleBackColor = true;
+            this.Valider.Click += new System.EventHandler(this.Valider_Click);
+            // 
+            // nomTxtBox
+            // 
+            this.nomTxtBox.Location = new System.Drawing.Point(123, 96);
+            this.nomTxtBox.Name = "nomTxtBox";
+            this.nomTxtBox.Size = new System.Drawing.Size(205, 20);
+            this.nomTxtBox.TabIndex = 6;
+            // 
+            // prenomTextBox
+            // 
+            this.prenomTextBox.Location = new System.Drawing.Point(123, 122);
+            this.prenomTextBox.Name = "prenomTextBox";
+            this.prenomTextBox.Size = new System.Drawing.Size(205, 20);
+            this.prenomTextBox.TabIndex = 7;
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Location = new System.Drawing.Point(123, 148);
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(205, 20);
+            this.noteTextBox.TabIndex = 8;
+            // 
+            // villeTextBox
+            // 
+            this.villeTextBox.Location = new System.Drawing.Point(123, 174);
+            this.villeTextBox.Name = "villeTextBox";
+            this.villeTextBox.Size = new System.Drawing.Size(205, 20);
+            this.villeTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "nom";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "prenom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "note";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "ville";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "liste";
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(343, 253);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(0, 13);
+            this.Message.TabIndex = 15;
+            this.Message.Click += new System.EventHandler(this.Message_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 310);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.villeTextBox);
+            this.Controls.Add(this.noteTextBox);
+            this.Controls.Add(this.prenomTextBox);
+            this.Controls.Add(this.nomTxtBox);
+            this.Controls.Add(this.Valider);
+            this.Controls.Add(this.Annuler);
+            this.Controls.Add(this.Supprimer);
+            this.Controls.Add(this.Modifier);
+            this.Controls.Add(this.Ajouter);
+            this.Controls.Add(this.listEtudiant);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox listEtudiant;
+        private System.Windows.Forms.Button Ajouter;
+        private System.Windows.Forms.Button Modifier;
+        private System.Windows.Forms.Button Supprimer;
+        private System.Windows.Forms.Button Annuler;
+        private System.Windows.Forms.Button Valider;
+        private System.Windows.Forms.TextBox nomTxtBox;
+        private System.Windows.Forms.TextBox prenomTextBox;
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.TextBox villeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Message;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        enum action { ADDING, DELETING, MODIFYING }
+        static string oradb = "Data Source =(DESCRIPTION =" +
+            "(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))" +
+            "(CONNECT_DATA =" +
+            "(SERVER = DEDICATED)" +
+            "(SERVICE_NAME = orcl)));" +
+            "User Id=c##usernam;" +
+            "Password=password;";
+        OracleConnection conn = new OracleConnection(oradb);
+        OracleCommand cmd = new OracleCommand(oradb);
+        int currentaction;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn.Open();
+            Updatelist();
+            BackToVisualisation();
+            conn.Close();
+
+        }
+
+        private void listEtudiant_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BackToVisualisation();
+            conn.Open();
+            cmd.CommandText = "Select nom,prenom,note,ville FROM students WHERE idstudent = " + listEtudiant.SelectedItem.ToString();
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    nomTxtBox.Text = dr["nom"].ToString();
+                    prenomTextBox.Text = dr["prenom"].ToString();
+                    noteTextBox.Text = dr["note"].ToString();
+                    villeTextBox.Text = dr["ville"].ToString();
+                }
+                
+            }
+            conn.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Message_Click(object sender, EventArgs e)
+        {
+
+        }
+  
+        private void Valider_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            if (currentaction == (int)action.ADDING)
+            {
+                
+                if (!(string.IsNullOrEmpty(nomTxtBox.Text) || string.IsNullOrEmpty(prenomTextBox.Text) || string.IsNullOrEmpty(noteTextBox.Text)))
+                {
+                    cmd.CommandText = "Insert into students(idstudent, nom, prenom, note, ville) Values(STUD_SEQ.nextval,'" +
+                        nomTxtBox.Text + "','" +
+                        prenomTextBox.Text + "','" +
+                        noteTextBox.Text + "','" +
+                        villeTextBox.Text + "')";
+                    cmd.ExecuteReader();
+                    Updatelist();
+                    BackToVisualisation();
+                }
+                else
+                {
+                    Message.Text = "veuillez remplir tous les bloques";
+                    Message.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            if (currentaction == (int)action.DELETING)
+            {
+                cmd.CommandText = "Delete from students where idstudent =" + listEtudiant.SelectedItem.ToString();
+                cmd.ExecuteReader();
+                Updatelist();
+                BackToVisualisation();
+            }
+            if (currentaction == (int)action.MODIFYING)
+            {
+
+                if (!(string.IsNullOrEmpty(nomTxtBox.Text) || string.IsNullOrEmpty(prenomTextBox.Text) || string.IsNullOrEmpty(noteTextBox.Text)))
+                {
+                    cmd.CommandText = "Update students set " + 
+                        " nom = '" + nomTxtBox.Text + "',"+
+                        " prenom = '" + prenomTextBox.Text + "'," +
+                        " note = '" + noteTextBox.Text + "'," +
+                        " ville = '" + villeTextBox.Text + "'" +
+                        " Where idstudent = " + listEtudiant.SelectedItem.ToString();
+                    cmd.ExecuteReader();
+                    Updatelist();
+                    BackToVisualisation();
+                }
+                else
+                {
+                    Message.Text = "veuillez remplir tous les bloques";
+                    Message.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            conn.Close();
+        }
+
+        private void Annuler_Click(object sender, EventArgs e)
+        {
+            BackToVisualisation();
+        }
+
+        private void Supprimer_Click(object sender, EventArgs e)
+        {
+            GoToVisualisation();
+            nomTxtBox.Enabled = false;
+            prenomTextBox.Enabled = false;
+            noteTextBox.Enabled = false;
+            villeTextBox.Enabled = false;
+            listEtudiant.Enabled = false;
+            currentaction = (int)action.DELETING;
+            Message.Text = "Cliquer sur Valider pour confirmer la suppression";
+            Message.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void Modifier_Click(object sender, EventArgs e)
+        {
+            GoToVisualisation();
+            nomTxtBox.Enabled = true;
+            prenomTextBox.Enabled = true;
+            noteTextBox.Enabled = true;
+            villeTextBox.Enabled = true;
+            listEtudiant.Enabled = false;
+            currentaction = (int)action.MODIFYING;
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            GoToVisualisation();
+            CleanTxtBoxes();
+            nomTxtBox.Enabled = true;
+            prenomTextBox.Enabled = true;
+            noteTextBox.Enabled = true;
+            villeTextBox.Enabled = true;
+            listEtudiant.Enabled = false;
+            currentaction = (int)action.ADDING;
+        }
+        private void CleanTxtBoxes()
+        {
+            nomTxtBox.Text = "";
+            prenomTextBox.Text = "";
+            noteTextBox.Text = "";
+            villeTextBox.Text = "";
+        }
+        private void GoToVisualisation()
+        {
+            Valider.Enabled = true;
+            Annuler.Enabled = true;
+            Supprimer.Enabled = false;
+            Modifier.Enabled = false;
+            Ajouter.Enabled = false;
+        }
+        private void BackToVisualisation()
+        {
+            Valider.Enabled = false;
+            Annuler.Enabled = false;
+
+            Ajouter.Enabled = true;
+            nomTxtBox.Enabled = false;
+            prenomTextBox.Enabled = false;
+            noteTextBox.Enabled = false;
+            villeTextBox.Enabled = false;
+            listEtudiant.Enabled = true;
+            listEtudiant.Text = "--- Select ---";
+            if (listEtudiant.SelectedItem == null)
+            {
+                Supprimer.Enabled = false;
+                Modifier.Enabled = false;
+            }
+            else
+            {
+
+                Supprimer.Enabled = true;
+                Modifier.Enabled = true;
+            }
+            currentaction = -1;
+        }
+        void Updatelist()
+        {
+            listEtudiant.Items.Clear();
+            cmd.CommandText = "select * from students";
+            cmd.Connection = conn;
+            
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    listEtudiant.Items.Add(dr["idstudent"]);
+                }
+
+            }
+        }
+    }
+}
